3b617d40d1d3fe4f342e4db6cd4ec3c20295e2c9
unknown <bdccordova@utpl.edu.ec>
2024-05-15 15:59:30 -0500

    CAMBIOS

--- EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
@@ -12,34 +12,20 @@ import paquete01.LibretaCalificacion;
  *
  * @author reroes
  */
-public class Ejecutor {
+public class Ejecutor2 {
 
     public static void main(String[] args) {
         // listado de variables para ingreso de datos por teclado
-        Scanner entrada = new Scanner(System.in);
-        String nombreEstudiante;
-        double[] calificacionesEstudiante;
-        int numerocalificaciones;
-
-        // Ingreso de valores por teclado
-        System.out.println("Ingrese el nombre del estudiante");
-        nombreEstudiante = entrada.nextLine();
-        System.out.println("Ingrese el número de calificaciones");
-        numerocalificaciones = entrada.nextInt();
-        // con el numerocalificaciones se crea el objeto arreglo de
-        // calificaciones tipo double
-        calificacionesEstudiante = new double[numerocalificaciones];
-
-        for (int i = 0; i < numerocalificaciones; i++) {
-            System.out.printf("Ingrese calificación %d\n", i + 1);
-            calificacionesEstudiante[i] = entrada.nextDouble();
-        }
-
+        
+        double[] notas ={10,9,8,10};
+        String nombreEstudiante = "René Elizalde";
+        
+       
         // con el ingreso del nombre del estudiante y el ingreso del conjunto
         // de calificaciones; se procede a crear el objeto de tipo
         // LibretaCalificacion
         LibretaCalificacion libreta = new LibretaCalificacion(nombreEstudiante,
-                calificacionesEstudiante);
+                notas);
         // Se llama a los métodos que realizan el calculo del
         // promedio y promedio cualitativo
         libreta.establecerPromedio();
